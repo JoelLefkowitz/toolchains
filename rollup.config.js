04e89af29b82b51b5f49626568bc6a7ff10e1fc8
Joel Lefkowitz <joel.lefkowitz@teya.com>
2025-01-04 10:38:50 +0000

    Add build hooks

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,24 @@
 const { nodeResolve } = require("@rollup/plugin-node-resolve");
+const terser = require("@rollup/plugin-terser");
 const commonjs = require("@rollup/plugin-commonjs");
-const clear = require("rollup-plugin-delete");
+const { spawnSync } = require("child_process");
+const { globSync } = require("glob");
+const fs = require("fs");
+const { unary } = require("ramda");
 
 module.exports = {
   input: "dist/main.js",
   output: {
-    file: "dist/main.js",
+    file: "dist/bundle.js",
     format: "cjs",
   },
   treeshake: "smallest",
   plugins: [
+    { buildStart: () => fs.rmdirSync("dist", { recursive: true }) },
+    { buildStart: () => spawnSync("npx", ["tsc"], { stdio: "inherit" }) },
     nodeResolve({ preferBuiltins: true }),
     commonjs({ ignoreDynamicRequires: true }),
-    clear({
-      hook: "buildEnd",
-      targets: ["dist"],
-      verbose: true,
-    }),
+    terser(),
+    { buildEnd: () => globSync("dist/main.*").map(unary(fs.rmSync)) },
   ],
 };
